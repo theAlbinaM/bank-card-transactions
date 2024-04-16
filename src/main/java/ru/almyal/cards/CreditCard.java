3388a6362fa2de0bbb7bc1118de91df42f35b453
theAlbinaM <thealina17m@gmail.com>
2024-04-16 21:04:28 +0300

    minor changes

--- src/main/java/ru/almyal/cards/CreditCard.java
+++ src/main/java/ru/almyal/cards/CreditCard.java
@@ -1,9 +1,6 @@
 package ru.almyal.cards;
 
 import java.util.Map;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CreditCard extends BankCard {
     private final double creditLimit;
